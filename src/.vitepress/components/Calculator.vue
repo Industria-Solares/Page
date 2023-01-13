898e425a1462c78b7548a682ad1f61ee5a51a15a
Bartosz Gorol <bartosz.gorol@abcfinlab.de>
2023-01-13 22:04:27 +0100

    Calculator

--- src/.vitepress/components/Calculator.vue
+++ src/.vitepress/components/Calculator.vue
@@ -8,8 +8,26 @@
             <v-text-field label="Höhe" variant="outlined"></v-text-field>
         </v-col>
     </v-row>
-    <v-btn @click="onClick" variant="outlined">Increase</v-btn>
-    <p>Count: {{ count }}</p>
+    <v-label>Modul Fläche</v-label>
+    <v-row>
+        <v-col cols="6">
+            <v-text-field label="Breite" variant="outlined"></v-text-field>
+        </v-col>
+        <v-col cols="6">
+            <v-text-field label="Höhe" variant="outlined"></v-text-field>
+        </v-col>
+    </v-row>
+    <v-label>Modul Leistung (kWh/Jahr)</v-label>
+    <v-row>
+        <v-col cols="6">
+            <v-text-field label="Leistung (kWh/Jahr)" variant="outlined"></v-text-field>
+        </v-col>
+    </v-row>
+
+
+
+    <v-btn @click="onClick" variant="outlined">Erhöhe</v-btn>
+    <p>Zähler: {{ count }}</p>
 </template>
 
 <script setup lang="ts">
