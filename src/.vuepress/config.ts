5472c322c63becbdd3a8a5f98e353b2993c9f1cc
Bartosz Gorol <bartosz.gorol@abcfinlab.de>
2023-01-14 15:36:49 +0100

    vitepress -> vuepress

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,16 @@
 import { defineUserConfig } from 'vuepress'
+import { viteBundler } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'de-DE',
   title: 'Industria Solares',
   description: 'Awesome Solar Power',
+  base: '/page/',
+  bundler: viteBundler({
+    viteOptions: {
+      ssr: {
+        noExternal: ['vuetify']
+      }
+    }
+  }),
 })
